9642a105bd3df43f3157e338fe7cd61dca757f68
xiang.liao <438944209@qq.com>
2024-05-18 11:52:29 +0800

    init

--- IoGame/iogame-common/src/main/java/cn/xwlin/common/action/CacheKeyConst.java
+++ IoGame/iogame-common/src/main/java/cn/xwlin/common/action/CacheKeyConst.java
@@ -8,10 +8,14 @@ public class CacheKeyConst {
   /**
    * 用户当前位置
    */
-  private static final String SERVER_REGION_MAP_USER_POSITION = "SERVER_{}_REGION_{}_MAP_{}_POSITION";
+  private static final String SERVER_REGION_MAP_USER_POSITION = "SERVER_%s_REGION_%s_MAP_%s_POSITION";
 
 
   public static String playPositionCacheKey(Long serverId, Long regionId, Long userId) {
     return String.format(SERVER_REGION_MAP_USER_POSITION, serverId, regionId, userId);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(playPositionCacheKey(1L, 1L, 1L));
   }
 }
