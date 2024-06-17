387e33724922d92ba52f899638b5ec5a1bfc9d83
廖祥 <xiang.liao@ly.com>
2024-06-17 19:21:10 +0800

    多数据源starter

--- BootStart/src/main/java/cn/xwlin/controller/RedisController.java
+++ BootStart/src/main/java/cn/xwlin/controller/RedisController.java
@@ -1,8 +1,6 @@
 package cn.xwlin.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("redis")
-public class RedissonController {
+public class RedisController {
 
-  @Autowired
-  private RedissonClient redissonClient;
   @Autowired
   private StringRedisTemplate redisTemplate;
 
@@ -29,19 +25,4 @@ public class RedissonController {
     return redisTemplate.opsForValue().get(key);
   }
 
-  @RequestMapping("lockTest")
-  public String lockTest(String lockKey) throws Exception {
-    RLock lock = redissonClient.getLock(lockKey);
-    boolean b = lock.tryLock();
-    if (b) {
-      log.info("#LockSuccess#lockKey:" + lockKey);
-    }
-    try {
-      Thread.sleep(5000);
-    } catch (Throwable t) {
-    } finally {
-      lock.unlock();
-    }
-    return "LockSuccess";
-  }
 }
