82c2bf84e984658b993206f9040a42d9ecaed8e9
廖祥 <xiang.liao@ly.com>
2024-06-20 17:38:33 +0800

    pageHelper

--- 框架类/config-server/src/main/java/cn/xwlin/configcenter/controller/ConfigCenterApiController.java
+++ 框架类/config-server/src/main/java/cn/xwlin/configcenter/controller/ConfigCenterApiController.java
@@ -1,5 +1,6 @@
 package cn.xwlin.configcenter.controller;
 
+import cn.dev33.satoken.annotation.SaIgnore;
 import cn.xwlin.configcenter.holder.ConfigChangeRequestHolder;
 import cn.xwlin.configcenter.service.ConfigService;
 import cn.xwlin.configcenter.service.SysConfigService;
@@ -27,21 +28,25 @@ public class ConfigCenterApiController {
   private SysConfigService sysConfigService;
 
   @RequestMapping("/checkAppModule")
+  @SaIgnore
   public HttpResp checkAppModule(String appCode, String moduleCode) {
     return configService.checkAppModule(appCode, moduleCode);
   }
 
   @RequestMapping("/getSysConfig")
+  @SaIgnore
   public HttpResp<GetConfigData> getSysConfig(String appCode, String moduleCode) {
     return sysConfigService.getSysConfig(appCode, moduleCode);
   }
 
   @RequestMapping("/getAllConfig")
+  @SaIgnore
   public HttpResp<GetConfigData> getAllConfig(String appCode, String moduleCode) {
     return configService.getAllConfig(appCode, moduleCode);
   }
 
   @RequestMapping("/refreshConfig")
+  @SaIgnore
   public DeferredResult<HttpResp<GetConfigData>> sayHello(String appCode, String moduleCode, String ip, long lastFetchTime, Long requestTimeout) {
     DeferredResult<HttpResp<GetConfigData>> result = new DeferredResult<>(requestTimeout - 5000);
     configChangeRequestHolder.addHolder(appCode, moduleCode, ip, lastFetchTime, result);
