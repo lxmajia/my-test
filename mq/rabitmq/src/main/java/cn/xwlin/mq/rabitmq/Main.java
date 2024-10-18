55652770e7679f2e43ec8fd75759bfac099ef222
xiang.liao <xiang.liao@ly.com>
2024-10-18 14:36:42 +0800

    init

---
+++ mq/rabitmq/src/main/java/cn/xwlin/mq/rabitmq/Main.java
@@ -0,0 +1,15 @@
+package cn.xwlin.mq.rabitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiang.liao
+ * @create 2024/10/16
+ */
+@SpringBootApplication
+public class Main {
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+}
